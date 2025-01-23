967bd9e9c3624910bb44b58a1e5813bc44ad5f72
Jihoon Lee <jnu21jihoon@gmail.com>
2025-01-24 01:28:16 +0900

    중복되는 코드(메소드)를 util폴더안에 restaurants-data.js파일에 따로 정의했다. 이러한 리팩토링을 통해서 app.js를 깔끔하게 만들었다. 이과정에서 module.exports 등을 배웠다.

--- code/00-starting-project/app.js
+++ code/00-starting-project/app.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 const express = require("express");
 const uuid = require("uuid");
 
+const resData = require("./util/restaurants-data");
+
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
@@ -17,14 +19,11 @@ app.get("/", function (req, res) {
 });
 
 app.get("/restaurants", function (req, res) {
-  const filePath = path.join(__dirname, "data", "restaurants.json");
-
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
+  const restaurants = resData.getStoredRestaurants();
 
   res.render("restaurants", {
-    numberOfRestaurants: storedRestaurants.length,
-    restaurants: storedRestaurants,
+    numberOfRestaurants: restaurants.length,
+    restaurants: restaurants,
   });
 });
 
@@ -32,11 +31,9 @@ app.get("/restaurants", function (req, res) {
 app.get("/restaurants/:id", function (req, res) {
   const restaurantId = req.params.id;
 
-  const filePath = path.join(__dirname, "data", "restaurants.json");
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
+  const restaurants = resData.getStoredRestaurants();
 
-  for (const restaurant of storedRestaurants) {
+  for (const restaurant of restaurants) {
     if (restaurant.id === restaurantId) {
       return res.render("restaurant-detail", {
         rid: restaurantId,
@@ -55,13 +52,10 @@ app.get("/recommend", function (req, res) {
 app.post("/recommend", function (req, res) {
   const restaurant = req.body;
   restaurant.id = uuid.v4();
-  const filePath = path.join(__dirname, "data", "restaurants.json");
 
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
-
-  storedRestaurants.push(restaurant);
-  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+  const restaurants = resData.getStoredRestaurants();
+  restaurants.push(restaurant);
+  resData.storeRestaurants(restaurants);
 
   res.redirect("/confirm");
 });
